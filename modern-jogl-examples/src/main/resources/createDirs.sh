4cda801721694b01e4326ee550eb350d45a492db
Ali Can Albayrak <albayrakalican@gmail.com>
2016-09-25 10:29:08 +0300

    maven prj initial commit

---
+++ modern-jogl-examples/src/main/resources/createDirs.sh
@@ -0,0 +1,61 @@
+mkdir -p tut03/cpuPositionOffset/shaders
+mkdir -p tut03/fragChangeColor/shaders
+mkdir -p tut03/shaderCalcOffset/shaders
+mkdir -p tut03/shaderPositionOffset/shaders
+
+mkdir -p tut04/aspectRatio/shaders
+mkdir -p tut04/aspectRatio/shaders
+mkdir -p tut04/orthoCube/shaders
+mkdir -p tut04/shaderPerspective/shaders
+
+mkdir -p tut05/vertexClipping/shaders
+mkdir -p tut05/overlapNoDepth/shaders
+mkdir -p tut05/depthClamping/shaders
+mkdir -p tut05/depthClamping/shaders
+mkdir -p tut05/depthBuffer/shaders
+mkdir -p tut05/baseVertexOverlap/shaders
+mkdir -p tut06/hierarchy/shaders
+mkdir -p tut06/rotations/shaders
+mkdir -p tut06/scale/shaders
+mkdir -p tut06/translation/shaders
+mkdir -p tut07/worldScene/shaders
+mkdir -p tut07/data
+mkdir -p tut07/worldWithUBO/shaders
+mkdir -p tut07/data
+
+
+mkdir -p tut08/cameraRelative/shaders
+mkdir -p tut08/data
+mkdir -p tut08/gimbalLock/shaders 
+mkdir -p tut08/interpolation/shaders
+mkdir -p tut08/quaternionYPR/shaders
+mkdir -p tut09/ambientLighting/shaders
+mkdir -p tut09/data
+mkdir -p tut09/basicLighting/shaders 
+mkdir -p tut09/scaleAndLighting/shaders
+mkdir -p tut10/fragmentAttenuation/shaders
+mkdir -p tut10/fragmentPointLighting/shaders
+mkdir -p tut10/vertexPointLighting/shaders
+mkdir -p tut11/blinnVsPhongLighting/shaders 
+mkdir -p tut11/gaussianSpecularLighting/shaders
+mkdir -p tut11/phongLighting/shaders
+mkdir -p tut11/data
+mkdir -p tut12/sceneLighting/shaders
+mkdir -p tut12/data
+mkdir -p tut13/basicImpostor/shaders
+mkdir -p tut13/data
+mkdir -p tut13/geomImpostor/shaders
+mkdir -p tut14/basicTexture/shaders
+mkdir -p tut14/data
+mkdir -p tut14/materialTexture/shaders
+mkdir -p tut14/data
+mkdir -p tut14/perspectiveInterpolation/shaders
+mkdir -p tut14/data
+
+
+mkdir -p tut15/manyImages/data
+mkdir -p tut15/data
+mkdir -p tut16/gammaCheckers/data
+mkdir -p tut16/gammaCheckers/shaders
+mkdir -p tut16/gammaRamp/shaders
+mkdir -p tut16/gammaRamp/data
